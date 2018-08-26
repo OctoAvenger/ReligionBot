630cc271654300951d3811446939106862b655cf
Nathan <rxbrglaughlin@gmail.com>
2018-08-26 15:13:15 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,14 +15,14 @@ bot.on("message", (message) => {
             embed.setDescription("**General commands:**\n" + prefix + "help *Displays this message.*\n\n**ALL COMMANDS AS OF VERSION 0.0.1**");
             message.channel.send({ embed });
     }
-        if (message.content == prefix + "religions") {
+    if (message.content == prefix + "religions") {
         message.channel.send({embed: {
             title: "Assignable Roles:\n\n",
             color: 0xF1DA75,
             fields: [
             { name: "Abrahamic:", value: "Christian\nMuslim\nJewish\nBahá'í", inline: true},
             { name: "Eastern:", value: "Hindu\nBuddhist\nTaoist\nConfucian\nSikhi", inline: true},
-            { name: "Other:", value: "Unitarian Universalist\nIrreligious\nPagan\nDeist\nGnostic\nPanentheist\nOther\n\n**For denomination roles do .[religion] e.g. .christian to see Christian denominations.**\n\nThe current religions with availiable denominations are: **Christian, Muslim, Irreligious, Hindu, Buddhist and Jewish**\n\nContact a moderator if there is a role you think should be added.", inline: true}
+            { name: "Other:", value: "Unitarian Universalist\nIrreligious\nPagan\nDeist\nGnostic\nPanentheist\nOther\n\n**For denomination roles do \`.[religion]\` e.g. \`.christian\` to see Christian denominations.**\n\nThe current religions with availiable denominations are: **Christian, Muslim, Irreligious, Hindu, Buddhist and Jewish**\n\nContact a moderator if there is a role you think should be added.", inline: true}
             ]
           }
         });
@@ -90,6 +90,60 @@ bot.on("message", (message) => {
     }
     });
 }
+    if (message.content.startsWith(prefix + 'kick')) {
+        const user = message.mentions.users.first();
+        if (user) {
+            const member = message.guild.member(user);
+            const logs = member.guild.channels.find('name', 'mod-log');
+            if (member) {
+                member.kick(`I don\'t know ask the moderator that kicked him.`).then(() => {
+                message.reply(`Successfully kicked ${user}`);
+                logs.send({embed: {
+                    title: "User kicked:\n\n",
+                    color: 0xaa3333,
+                    fields: [
+                        { name: "Username:", value: `${member}\n\nUser ID: ${member.id}`, inline: true},
+                        { name: "reason", value: `null`, inline: true},
+                    ]
+                    }
+                });
+                }).catch(err => {
+                message.reply('I was unable to kick the member. `' + err + '`');
+            });
+        }   else {
+          message.reply('That user doesn\'t exist!');
+        }
+      } else {
+        message.reply('That\'s nice but who do you want to kick?');
+      }
+    }
+    if (message.content.startsWith(prefix + 'ban')) {
+        const user = message.mentions.users.first();
+        if (user) {
+            const member = message.guild.member(user);
+            const logs = member.guild.channels.find('name', 'mod-log');
+            if (member) {
+                member.ban(`I don\'t know ask the moderator that kicked him.`).then(() => {
+                message.reply(`Successfully banned ${user}`);
+                logs.send({embed: {
+                    title: "User banned:\n\n",
+                    color: 0xaa3333,
+                    fields: [
+                        { name: "Username:", value: `${member}\n\nUser ID: ${member.id}`, inline: true},
+                        { name: "reason", value: `null`, inline: true},
+                    ]
+                    }
+                });
+                }).catch(err => {
+                message.reply('I was unable to ban the member. `' + err + '`');
+            });
+        }   else {
+          message.reply('That user doesn\'t exist!');
+        }
+      } else {
+        message.reply('That\'s nice but who do you want to ban?');
+      }
+    }
     
     /*if (new Date().getHours() == '5' && new Date().getMinutes() == '00') {
         console.log("It worked");
@@ -101,14 +155,13 @@ bot.on("message", (message) => {
 
 bot.on('guildMemberAdd', member => {
     const unverified = member.guild.channels.find('name', 'unverified');
-    const logs = member.guild.channels.find('name', 'staff-logs')
+    const logs = member.guild.channels.find('name', 'join-log')
 
     if (!unverified) return;
-    unverified.send(`Welcome ${member} to **Religious Online Discussions**! We are a server dedicated to Interfaith cooperation to study religion and other philosophies to bring us closer to any Power we believe in whether that be one God, many gods, no god, etc.\nTo get yourself started please do ".religions" to check out the roles we have and do ".iam [role]" to add the role to yourself.\nContact a staff member if you need assistence and enjoy your time here.`);
+    unverified.send(`Welcome ${member} to **Religious Online Discussions**! We are a server dedicated to Interfaith cooperation to study religion and other philosophies to bring us closer to any Power we believe in whether that be one God, many gods, no god, etc.\nTo get yourself started please do \`.religions\` to check out the roles we have and do \`.iam [role]\` to add the role to yourself.\nContact a staff member if you need assistence and enjoy your time here.`);
     logs.send({embed: {
         title: "User joined:\n\n",
         color: 0x66ba67,
-        /*thumbnail: member.user.displayAvatarURL,*/
         fields: [
             { name: "Username:", value: `${member}`, inline: true},
             { name: "Joined server:", value: `${member.joinedAt}`, inline: true},
@@ -118,7 +171,7 @@ bot.on('guildMemberAdd', member => {
     });
   });
 bot.on('guildMemberRemove', member => {
-    const logs = member.guild.channels.find('name', 'staff-logs')
+    const logs = member.guild.channels.find('name', 'join-log')
     logs.send({embed: {
         title: "User left:\n\n",
         color: 0xaa3333,
