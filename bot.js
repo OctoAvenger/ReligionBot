da009238ee8e406324970ce4aa98c9de5e27bdb5
Nathan <rxbrglaughlin@gmail.com>
2018-10-20 18:15:16 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
- //Defines bot/prefix and languages used.
+//Defines bot/prefix and languages used.
 const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
@@ -39,7 +39,7 @@ bot.on("message", (message) => {
             color: 0xF1DA75,
             fields: [
             { name: "Abrahamic:", value: "Christian\nMuslim\nJewish\nBahá'í", inline: true},
-            { name: "Eastern:", value: "Hindu\nBuddhist\nDaoist\nConfucian\nSikhi", inline: true},
+            { name: "Eastern:", value: "Hindu\nBuddhist\nTaoist\nConfucian\nSikhi", inline: true},
             { name: "Other:", value: "Unitarian Universalist\nIrreligious\nPagan\nGnostic\nPanentheist\nOther\n\n**To see available denomination roles do \`.denominations\`**", inline: true}
             ]
           }
@@ -150,7 +150,7 @@ bot.on("message", (message) => {
     if (message.content.startsWith(prefix + 'delete')) {
         if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)))
         return message.reply("Sorry, you don't have permissions to use this!");
-            const logs = message.member.guild.channels.find('name', 'mod-log');
+            const logs = message.member.guild.channels.find('name', 'delete-log');
             message.delete(100).then(() => {
             message.channel.bulkDelete(message.content.slice(8, message.content.length)).then(() => {
             message.reply(`Successfully deleted \`${message.content.slice(8, message.content.length)}\` messages`).then(msg => msg.delete(3000));
